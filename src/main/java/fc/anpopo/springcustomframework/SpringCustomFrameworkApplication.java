07b1d2a6a2c20e248cbcb0482fc4fb703c5242d9
ansehyeong <dkstpgud@gmail.com>
2023-01-20 18:17:03 +0900

    initialize

---
+++ src/main/java/fc/anpopo/springcustomframework/SpringCustomFrameworkApplication.java
@@ -0,0 +1,13 @@
+package fc.anpopo.springcustomframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCustomFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCustomFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fc/anpopo/springcustomframework/SpringCustomFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package fc.anpopo.springcustomframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCustomFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
